0b5f132899726fb4cbbc4596c94ef22139628c71
nowhereOnce <alejandro.aguilar.fi02@gmil.com>
2025-02-15 16:02:39 -0600

    initialize React app with Vite, Tailwind CSS, and ESLint setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import LandingPage from './components/LandingPage'
+
+function App() {
+
+  return (
+    <>
+      <LandingPage></LandingPage>
+    </>
+  )
+}
+
+export default App
