084b25b1154710a0adf820bc30445d7d4ae0b342
nowhereOnce <alejandro.aguilar.fi02@gmail.com>
2025-07-16 22:08:40 -0600

    Added the url to the shader gradient, this will be a brand new background

--- src/components/MePage.jsx
+++ src/components/MePage.jsx
@@ -10,6 +10,8 @@ const options = [
   { value: 'long_term', label: '12 months' }
 ]
 
+/* https://www.shadergradient.co/customize?animate=on&axesHelper=on&bgColor1=%23000000&bgColor2=%23000000&brightness=0.2&cAzimuthAngle=180&cDistance=3.9&cPolarAngle=115&cameraZoom=1&color1=%231DB954&color2=%23FFDD57&color3=%23000000&destination=onCanvas&embedMode=off&envPreset=city&format=gif&fov=45&frameRate=10&grain=off&lightType=3d&pixelDensity=1&positionX=-0.5&positionY=0.1&positionZ=0&range=enabled&rangeEnd=40&rangeStart=0&reflection=0.1&rotationX=0&rotationY=0&rotationZ=235&shader=defaults&type=waterPlane&uAmplitude=0&uDensity=0.6&uFrequency=5.5&uSpeed=0.08&uStrength=2.4&uTime=0.2&wireframe=false */
+
 export default function MePage() {
 
     const [selectedOption, setSelectedOption] = useState(options[0]); // Time range select
