51a9d7a03307b5e572ef8c300bbca7e8cbc71b47
nowhereOnce <alejandro.aguilar.fi02@gmail.com>
2025-08-07 12:33:00 -0600

    first version of a responsive landing page

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -5,11 +5,11 @@ function LandingPage() {
     return (
         <>
             <section className='min-h-screen flex justify-center flex-col bg-[url("/background-gradient.png")] bg-cover text-white'>
-                <div className='ml-40'>
-                    <h1 className='text-8xl font-bold '>BeatStat_</h1>
-                    <p className='text-3xl font-light my-10 w-2xl'>Unlock your Spotify story. Dive into your stats, playlists, and musical journey like never before.</p>
+                <div className='ml-10 m-auto sm:ml-12 md:ml-20 lg:ml-32 xl:ml-40'>
+                    <h1 className='text-6xl sm:text-5xl md:text-7xl lg:text-8xl xl:text-8xl font-bold'>BeatStat_</h1>
+                    <p className='text-lg sm:text-xl md:text-2xl lg:text-3xl font-light my-6 sm:my-8 md:my-10 max-w-xs sm:max-w-md md:max-w-lg lg:max-w-xl xl:max-w-2xl mb-20'>Unlock your Spotify story. Dive into your stats, playlists, and musical journey like never before.</p>
                     <button 
-                        className='mt-8 text-white p-6 border-2 rounded-full text-2xl hover:bg-white hover:text-black transition duration-300'
+                        className='mt-4 sm:mt-6 md:mt-8 text-white p-4 sm:p-5 md:p-6 border-2 rounded-full text-lg sm:text-xl md:text-2xl hover:bg-white hover:text-black transition duration-300'
                         onClick={handleLogin}
                     >
                         Get Started!
